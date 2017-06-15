12302464cfb2b9bb2bcaad7316e1de7db88d8475
Karen Vicente <karenyasmin37@gmail.com>
2017-06-15 14:38:09 -0300

    Subindo projeto pokedex

---
+++ meu-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuComponenteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
